357d2c4dcea8af2dc58258b6ab49de4ba481eda0
Chandrakant Mate <chandrakantmate24@gmail.com>
2026-03-24 07:55:24 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 1223322
+let accountEmail = "Ram24@gmail.com"
+var accountPassword = "23433"
+
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword])
+
