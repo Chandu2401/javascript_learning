ced4cb4cf1acdb6a2e51688c06d460465be6466a
Chandrakant Mate <chandrakantmate24@gmail.com>
2026-04-04 11:44:00 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,37 @@
+
+
+let text = "chandu";
+
+console.log(text.toUpperCase()); // CHANDU
+console.log(text.toLowerCase()); // chandu
+
+let text1 = "JavaScript";
+
+console.log(text1.slice(4, 10));
+
+
+let demo = "Hello world";
+
+console.log(demo.includes("Hello"));
+
+
+let name1 = "Chandu";
+
+console.log(name1);
+console.log(name1.length);
+
+
+let a = "10";
+let b = "5";
+
+console.log(a + b);
+
+
+let name = "Chandu";
+let age = 23;
+
+console.log(`My name is ${name} and I am ${age}`);
+
+let tex = "hello";
+
+console.log(tex.toUpperCase().slice(0,3));
